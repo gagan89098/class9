8ff7419197597d0aae5617d9658e080fbfc22849
gagan89098 <99317160+gagan89098@users.noreply.github.com>
2022-02-09 12:52:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+
+function setup() {
+  createCanvas(400,400);
+  ball=createSprite(200,200,20,20)
+}
+
+function draw() 
+{
+  background(30);
+  if(keyIsDown(UP_ARROW)){
+    ball.position.y=ball.position.y-5
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    ball.position.y=ball.position.y+5
+  }
+  if(keyIsDown(RIGHT_ARROW)){
+    ball.position.x=ball.position.x+5
+  }
+  if(keyIsDown(LEFT_ARROW)){
+    ball.position.x=ball.position.x-5
+  }
+drawSprites()
+}
+
+
+
+
